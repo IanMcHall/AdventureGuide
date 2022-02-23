d6b0b4311e7e949ee01dacf9bc47ae7c1f3bbd28
Ian Hall <ianmchall@yahoo.com>
2022-02-22 21:45:53 -0500

    first commit

---
+++ Character.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGuide
+{
+    public class Character
+    {
+        public string playerName { get; set; }
+        public string characterName { get; set; }
+        public int characterLevel { get; set; }
+        public string characterBackground { get; set; }
+        public string characterAlignment { get; set; }
+        public int characterEXP { get; set; }
+
+        public CharacterRace characterRace { get; set; }
+        public CharacterClass characterClass { get; set; }
+
+        public void GainEXP() //Make sure this can add incremental exp, and see if we can make the message reflect how much is gained.
+        {
+            characterEXP++;
+
+            Console.WriteLine($"{ characterName} gained experience!");
+        }
+
+        public void LevelUp()
+        {
+            characterLevel++;
+
+            Console.WriteLine($"{characterName} leveled up!");
+        }
+
+        public void DisplayCharacterDetails()
+        {
+            Console.WriteLine($"\nCharacter Name: {characterName}\nPlayer Name: {playerName}\nRace: {characterRace}\nClass: {characterClass}\nLevel: {characterLevel}\nBackground: {characterBackground}\nAlignment: {characterAlignment}\nEXP: {characterEXP}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AdventureGuide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //add list for character class
+
+            Console.WriteLine("What is your name?");
+                string playerName = Console.ReadLine();
+            Console.WriteLine("What is your character name?");
+                string characterName = Console.ReadLine();
+            Console.WriteLine("What is your character level?");
+                int characterLevel = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //creates new instance of classs pulling from user input
+            Character character = new Character();
+            character.playerName = playerName;
+            character.characterName = characterName;
+
+
+
+
+                
+            
+            
+            
+          
+            
+            
+        }
+    }
+}
